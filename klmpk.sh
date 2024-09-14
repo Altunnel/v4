294f0338eafec0a95ee26aaf61cab515e85dea13
Altunnel <155318569+Altunnel@users.noreply.github.com>
2024-09-15 01:15:12 +0700

    Add files via upload

--- klmpk.sh
+++ klmpk.sh
@@ -635,14 +635,13 @@ EOF
 clear
 function ins_swab(){
 clear
-print_install "Memasang Swap"
 gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
     gotop_link="https://github.com/xxxserxxx/gotop/releases/download/v$gotop_latest/gotop_v"$gotop_latest"_linux_amd64.deb"
     curl -sL "$gotop_link" -o /tmp/gotop.deb
     dpkg -i /tmp/gotop.deb >/dev/null 2>&1
     
         # > Buat swap
-    dd if=/dev/zero of=/swapfile bs=1024 count=2097152
+    dd if=/dev/zero of=/swapfile bs=2047 count=1048576
     mkswap /swapfile
     chown root:root /swapfile
     chmod 0600 /swapfile >/dev/null 2>&1
@@ -656,7 +655,6 @@ gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | 
     
     wget ${REPO}limit/bbr.sh &&  chmod +x bbr.sh && ./bbr.sh
     wget https://raw.githubusercontent.com/SARTAMP/src/main/udp/udp-custom.sh && chmod +x udp-custom.sh && ./udp-custom.sh
-print_success "Swap"
 }
 
 instalbot() {
