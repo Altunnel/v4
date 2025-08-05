c9f7bb1f183fa9e1b81c1409e8634e08b9c42c5e
Altunnel <155318569+Altunnel@users.noreply.github.com>
2025-08-06 05:05:53 +0700

    Update cf.sh

--- limit/cf.sh
+++ limit/cf.sh
@@ -6,7 +6,7 @@ apt install jq curl -y
 DOMAIN=ketujuh.my.id
 
 # Membuat subdomain secara acak dengan domain utama
-sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
+sub=vpn$(</dev/urandom tr -dc a-z0-9 | head -c7)
 dns=${sub}.$DOMAIN
 
 # Kredensial Cloudflare
